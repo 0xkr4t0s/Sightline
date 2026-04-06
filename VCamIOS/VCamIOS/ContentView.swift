1f8ddc7d8aecc5a1e1557a8376ee2cf1c9fe2ee2
0xKr4t0s <10257520+0xkr4t0s@users.noreply.github.com>
2026-04-07 02:03:50 +1000

    Initial Commit

---
+++ VCamIOS/VCamIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VCamIOS
+//
+//  Created by 0xKr4t0s on 7/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VCamIOS/VCamIOS/VCamIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  VCamIOSApp.swift
+//  VCamIOS
+//
+//  Created by 0xKr4t0s on 7/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct VCamIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
